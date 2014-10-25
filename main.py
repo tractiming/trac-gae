2ef0e472346913980b15eb6ed1b7a041cf2cb75c
elliot <ejhevel@gmail.com>
2014-10-25 15:50:55 -0500

    more settings fixes

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import os
 import sys
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.dev")
+os.environ["DJANGO_SETTINGS_MODULE"] = "settings.dev"
 
 from google.appengine.ext.webapp import util
 from django.conf import settings
-settings._target = None
+#settings._target = None
 
 import django.core.handlers.wsgi
 
+from django.conf import settings
+#print settings.SECRET_KEY
 application = django.core.handlers.wsgi.WSGIHandler()
 
