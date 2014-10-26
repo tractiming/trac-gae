2a93c146a83f165aec67ab1510e6cd6eb8ec59eb
elliot <ejhevel@gmail.com>
2014-10-25 22:21:51 -0500

    changing directory for browsable api static files

--- settings/common.py
+++ settings/common.py
@@ -158,7 +158,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
         normpath(join(DJANGO_ROOT, 'static')),
+        normpath(join(DJANGO_ROOT, 'libs', 'rest_framework', 'static')),
 )
+print STATICFILES_DIRS
 ##################################
 
 ########## TEMPLATE CONFIGURATION ##########
