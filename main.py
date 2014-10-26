570f86e92e4ca8bfdd0bf22144353bb69d8961b4
elliot <ejhevel@gmail.com>
2014-10-25 21:49:56 -0500

    and more

--- main.py
+++ main.py
@@ -7,10 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 import os
-#import django.core.handlers.wsgi
+import django.core.handlers.wsgi
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.dev")
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
-#os.environ["DJANGO_SETTINGS_MODULE"] = "settings.dev"
-#application = django.core.handlers.wsgi.WSGIHandler()
+os.environ["DJANGO_SETTINGS_MODULE"] = "settings.dev"
+application = django.core.handlers.wsgi.WSGIHandler()
