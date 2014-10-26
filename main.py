f8683de7cc5bbb209a085abe13ca896dec0408c5
elliot <ejhevel@gmail.com>
2014-10-25 21:45:59 -0500

    a few more

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 import os
-import sys
 #import django.core.handlers.wsgi
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.dev")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 #os.environ["DJANGO_SETTINGS_MODULE"] = "settings.dev"
