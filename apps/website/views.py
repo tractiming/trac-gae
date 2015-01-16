1e9545ba72500ce7f965dd49f7512f9c5fd56c1c
griffinkelly <griffinkelly2013@u.northwestern.edu>
2015-01-16 10:20:21 -0600

    Trying to fix register issue

--- apps/website/views.py
+++ apps/website/views.py
@@ -12,8 +12,8 @@ def login(request):
 def live_view(request):
     return render(request, 'liveView.html', {})
 
-#def register(request):
-#    return render(request, 'register.html', {})
+def register(request):
+    return render(request, 'register.html', {})
 
 def home(request):
     return render(request, 'home.html', {})
