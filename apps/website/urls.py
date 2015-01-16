404184a295c386dd620239b481b55c222b6c8dc6
griffinkelly <griffinkelly2013@u.northwestern.edu>
2015-01-16 10:25:10 -0600

    Register works. Revert back

--- apps/website/urls.py
+++ apps/website/urls.py
@@ -5,7 +5,7 @@ import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^login', views.login, name='login'),
-    url(r'^register', views.register, name='register'),
+    # url(r'^register', views.register, name='register'),
     url(r'^liveview', views.live_view, name='liveview'),
     url(r'^calendar', views.calendar, name='calendar'),
     url(r'^create', views.create, name='create'),
