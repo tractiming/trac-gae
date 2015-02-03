18af3d395c697738f59808eeb0a0dd34e4442d32
griffinkelly <griffinkelly2013@u.northwestern.edu>
2015-02-03 16:09:47 -0600

    revert. didn't work

--- apps/trac/models.py
+++ apps/trac/models.py
@@ -114,7 +114,7 @@ class TimingSession(models.Model):
 
                 # Convert the number to a string to prevent round-off error.
                 str_dt = str(round(dt.total_seconds(), 3))
-                interval.append([str_dt])
+                interval.append(str_dt)
                 counter = range(1,len(interval)+1)   
 
             # Filtering algorithm.
