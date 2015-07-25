5275fe5efa465d7e7785ce605979434fcefe5a66
elliot <ejhevel@gmail.com>
2015-07-25 10:29:45 -0500

    Add settings fix to the right file.

--- manage.py
+++ manage.py
@@ -4,12 +4,7 @@ import sys
 
 if __name__ == "__main__":
 
-    if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine'):
-        settings = "settings.common"
-    else:
-        settings = "settings.dev"
-
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings)
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.dev")
 
     from django.core.management import execute_from_command_line
 
