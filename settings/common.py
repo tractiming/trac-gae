dc52d6701a06b666add97476912868db0aa875e0
elliot <ejhevel@gmail.com>
2015-07-26 15:18:31 -0500

    Edit email settings

--- settings/common.py
+++ settings/common.py
@@ -219,7 +219,7 @@ WSGI_APPLICATION = 'main.application'
 ########### EMAIL CONFIG ###################
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'tracchicago@gmail.com'
-EMAIL_HOST_PASSWORD = 'trac2015'
+EMAIL_HOST_PASSWORD = 'bwemkibrtyxrksed'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 ############################################
