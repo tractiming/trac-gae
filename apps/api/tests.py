e57d2dd5321adcc0beaf06c4ae65e8fa10a96fce
elliot <ejhevel@gmail.com>
2015-08-29 11:19:03 -0500

    Random test to check jenkins.

--- apps/api/tests.py
+++ apps/api/tests.py
@@ -1,16 +1,42 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.utils import timezone
+from django.core.urlresolvers import reverse
+import mock
+from trac.models import *
+from api.views import *
+from rest_framework.test import APITestCase
+from provider.oauth2.models import Client, AccessToken
+from rest_framework.test import APIRequestFactory, force_authenticate
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+
+class TagViewSetTestCase(APITestCase):
+
+    fixtures = ['trac_min.json']
+
+
+    def test_get_queryset(self):
+        #url = reverse('api/tags')
+        #self.client.force_authenticate(user=User.objects.get(username='alsal'))
+        #resp = self.client.get('/tags/1')
+        #resp = TagViewSet(self.client)
+        #print resp.data
+        print(Tag.objects.all())
+        factory = APIRequestFactory()
+        request = factory.get('/api/tags', '', content_type='application/json')
+        user = User.objects.get(username='alsal')
+        view = TagViewSet.as_view(actions={'get': 'retrieve'})
+        #self.client.force_authenticate(user=user)
+        #resp = self.client.get('/api/tags/1/', format='json')
+        #print(resp)
+        #import pdb; pdb.set_trace()
+        #request = factory.get('/api/tags/')
+        #force_authenticate(request, user=user)
+        print user.is_authenticated
+        request.user = user
+        response = view(request)
+        print response.data
+
+
+    def test_something(self):
+        pass
