bdb2fee27a1a9571f5c48a6c0c703beb3de8f66c
griffinkelly <griffinkelly2013@u.northwestern.edu>
2015-09-15 01:40:13 -0500

    logout view has broken pipe, quick redirect, need to fix later

--- static/js/access_token.js
+++ static/js/access_token.js
@@ -4,22 +4,24 @@ if (sessionStorage.access_token == null) {
 }
 else{
 	$(document).ready(function() {
-		$('li a.logout').click(function(){			
-			$.ajax({
-				type:"POST",
-				url: '/api/logout/',
-				headers: {Authorization: 'Bearer ' + sessionStorage.access_token},
-				dataType: 'text',
-				success: function(data) {
+		$('li a.logout').click(function(){	
 					sessionStorage.clear();
-					location.href='/login';
-					},
-				error: function(xhr, errmsg, err) {
-					sessionStorage.clear();
-					location.href='/login';
-					}	
+					location.href='/login';		
+			//$.ajax({
+				//type:"POST",
+				//url: '/api/logout/',
+				//headers: {Authorization: 'Bearer ' + sessionStorage.access_token},
+				//dataType: 'text',
+				//success: function(data) {
+					//sessionStorage.clear();
+					//location.href='/login';
+					//},
+				//error: function(xhr, errmsg, err) {
+					//sessionStorage.clear();
+					//location.href='/login';
+					//}	
 					
-			});
+			//});
 		});
 	});
 }
