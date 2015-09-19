468e0d0fed3cdb0af74c1ae350228522f6382f79
elliot <ejhevel@gmail.com>
2015-09-18 23:23:45 -0500

    Manually copy six into libs.

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ pip install -r requirements/production.txt
 mkdir -p libs
 find venv/lib/python2.7/site-packages/ -maxdepth 1 -mindepth 1 \
     -not -name "*.egg*" -type d -exec cp -r {} libs \;
+cp venv/lib/python2.7/site-packages/six.py libs
 
 appcfg.py update .
 rm -rf libs
