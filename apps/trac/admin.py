2df35d526a9a94bef77d6bcb689f44096c3c89f0
elliot <ejhevel@gmail.com>
2015-12-05 22:21:21 -0600

    Register trac models for admin site

--- apps/trac/admin.py
+++ apps/trac/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from trac.models import TimingSession, Tag, Coach, Athlete, Team, Reader
 
-# Register your models here.
+admin.site.register(TimingSession)
+admin.site.register(Tag)
+admin.site.register(Coach)
+admin.site.register(Athlete)
+admin.site.register(Team)
+admin.site.register(Reader)
