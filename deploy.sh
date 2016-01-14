a33b568971ff30f266a5da4c79492842ced6425a
elliot <ejhevel@gmail.com>
2016-01-13 18:52:35 -0800

    DEV: Revert to old deploy script, but keep eggs

--- deploy.sh
+++ deploy.sh
@@ -1,60 +1,17 @@
 #!/bin/bash
 
-# This script installs all the Python dependencies that need to
-# be vendored into the application, then pushes the code to
-# AppEngine.
-
-rm -rf libs
-mkdir libs
-echo 'Deleted existing libs...'
-
-pip --version
-pip --help
-#pip freeze | xargs pip uninstall -y
+pip freeze | xargs pip uninstall -y
 pip install \
     --no-deps \
-    --upgrade \
-    --force-reinstall \
-    --install-option="--prefix=$(pwd)/libs" \
-    -I \
     -r requirements/production.txt
-echo 'Pip installed dependencies...'
 
-if [ -d libs ]; then
-    cd libs
-    mv lib/python2.7/site-packages/* .
-    rm -rf lib
-    rm -rf bin
-
-    # AppEngine will not upload both .py and .pyc versions of a
-    # file. Also note that gcloud requires us to keep the
-    # .egg-info files around :/
-    find . -name "*.pyc" -delete
-    find . -name "*.egg-info" -not -name "*gcloud*" | xargs rm -rf
-
-    # Delete some locale files to stay under the file limit.
-    rm -rf django/contrib/admin/locale/*
-
-    # If needed, we can zip some of the packages and then have
-    # AppEngine use zipimport. Not all modules can be zipped.
-    # For instance, Django has non .py locale files that need
-    # to be imported, and other packages have migrations in
-    # places that are expected to be unzipped directories.
-    #zip -9mrv libs.zip . -x \
-    #    django/\* \
-    #    oauth2_provider/\* \
-    #    djstripe/\* \
-    #    *.egg-info/\*
-    #
-    cd ..
-    ls -l
-    ls -l libs
-fi
-echo 'Created zip archive...'
+mkdir -p libs
+SITE_DIR=`python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())"`
+find $SITE_DIR/ -maxdepth 1 -mindepth 1  -type d -exec cp -r {} libs \;
+cp $SITE_DIR/six.py libs
 
 cp -r libs/django/contrib/admin/static/admin static/
 
-# Push changed to AppEngine.
 $GAE_DIR/google_appengine/appcfg.py \
     --oauth2_refresh_token=$REFRESH_TOKEN \
     update .
