183b17f83afdc8287d48e6d993c33fc98c9240c8
elliot <ejhevel@gmail.com>
2016-01-24 21:24:20 -0800

    MAINT: Fix imports in `auth_views`

--- apps/trac/views/auth_views.py
+++ apps/trac/views/auth_views.py
@@ -1,10 +1,12 @@
+import base64
 import json
 
 from django.core.mail import send_mail
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django.template import loader
-from oauth2_provider.models import Application, AccessToken
+from django.utils import timezone
+from oauth2_provider.models import AccessToken
 from oauth2_provider.views import TokenView as _TokenView
 from rest_framework import permissions, status
 from rest_framework.decorators import api_view, permission_classes
@@ -12,7 +14,7 @@ from rest_framework.response import Response
 
 from trac.models import User, Team
 from trac.serializers import AthleteSerializer, CoachSerializer, UserSerializer
-from trac.utils.user_util import user_type, is_coach
+from trac.utils.user_util import is_coach
 
 
 _serializer_lookup = {'athlete': AthleteSerializer, 'coach': CoachSerializer}
@@ -62,7 +64,7 @@ class TokenView(_TokenView):
     See `oauth2_provider/views/base.py`.
     """
     def post(self, request, *args, **kwargs):
-        url, headers, body, status = self.create_token_response(request)
+        _, headers, body, status = self.create_token_response(request)
         response = HttpResponse(content=body, status=status)
 
         for k, v in headers.items():
@@ -138,17 +140,17 @@ def reset_password(request):
       required: true
       type: string
     """
-    name =  base64.urlsafe_b64decode(request.POST.get('user').encode('utf-8'))
-    user = User.objects.get(pk = name)
+    name = base64.urlsafe_b64decode(request.POST.get('user').encode('utf-8'))
+    user = User.objects.get(pk=name)
     token = request.auth
     if token not in user.accesstoken_set.all():
-            return HttpResponse(status.HTTP_403_FORBIDDEN)
+        return HttpResponse(status.HTTP_403_FORBIDDEN)
     if token.expires < timezone.now():
-            return HttpResponse(status.HTTP_403_FORBIDDEN)
+        return HttpResponse(status.HTTP_403_FORBIDDEN)
     if user.is_authenticated():
         user.set_password(request.POST.get('password'))
         user.save()
     else:
         return HttpResponse(status.HTTP_403_FORBIDDEN)
-    user.accesstoken_set.get(token = token).delete()
+    user.accesstoken_set.get(token=token).delete()
     return HttpResponse(status.HTTP_200_OK)
