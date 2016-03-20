595850787637de7aa4b4f803858c61ad7aad8b21
griffinkelly <griffinkelly2013@u.northwestern.edu>
2016-03-19 22:14:49 -0500

    Start index changes for Geoff

--- apps/website/urls.py
+++ apps/website/urls.py
@@ -14,6 +14,8 @@ sitemaps = {
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='index.html'),
         name='index'),
+    url(r'^product', TemplateView.as_view(template_name='product.html'),
+        name='product'),
 
     url(r'^home', csrf_exempt(
         TemplateView.as_view(template_name='home.html')),
