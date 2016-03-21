560c267d10a9eaa782a75ebb3a858a4d2ea86ee1
griffinkelly <griffinkelly2013@u.northwestern.edu>
2016-03-20 22:59:16 -0500

    Add separate quote page

--- apps/website/urls.py
+++ apps/website/urls.py
@@ -16,6 +16,8 @@ urlpatterns = patterns('',
         name='index'),
     url(r'^product', TemplateView.as_view(template_name='product.html'),
         name='product'),
+    url(r'^quote', TemplateView.as_view(template_name='quote.html'),
+        name='quote'),
 
     url(r'^home', csrf_exempt(
         TemplateView.as_view(template_name='home.html')),
